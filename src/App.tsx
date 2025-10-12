189b67c1cd5b34492fede58f263d4fd802d4da1e
charliewturner <c.w.turner@hotmail.com>
2025-10-12 21:26:01 +0100

    Switch app to render Profile for CSS structuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import Homepage from "./components/Homepage/Homepage";
 
+import Profile from "./components/Profile/Profile";
+
 function App(): React.JSX.Element {
   return (
     <>
-      <Homepage></Homepage>
+      {/* <Homepage></Homepage> */}
+      <Profile></Profile>
     </>
   );
 }
